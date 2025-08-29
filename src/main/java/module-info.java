849d877d734231f40075c5467b27adf7dae419d4
Oliver Wolff <23139298+cuioss@users.noreply.github.com>
2025-08-29 16:09:40 +0200

    Initial Setup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-module de.cuioss.template {
-    exports de.cuioss.template;
+module de.cuioss.rewrite {
+    exports de.cuioss.rewrite;
 
     requires static lombok;
 }
--- src/test/java/de/cuioss/rewrite/HelloTest.java
+++ src/test/java/de/cuioss/rewrite/HelloTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.cuioss.template;
+package de.cuioss.rewrite;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
